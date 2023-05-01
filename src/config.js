f5c7d1c461d52c90e799deaa3598fb2e7d30049b
Goutham Kannan <54542723+GouthamKannan@users.noreply.github.com>
2023-05-01 19:15:35 +0530

    Update backend url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const configs = {
-    api_url : "https://to-do-application-backend.herokuapp.com"
+    // api_url : "https://to-do-application-backend.herokuapp.com"
+    api_url : "https://to-do-application-backend.onrender.com"
 }
 
 export default configs
